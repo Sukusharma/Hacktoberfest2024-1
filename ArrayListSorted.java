3f9d0f3295acc514d4ea723671f0a4377feb7070
Sukusharma <139940860+Sukusharma@users.noreply.github.com>
2024-10-25 00:00:27 +0530

    Create ArrayListSorted.java

---
+++ ArrayListSorted.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayListSorted{
+  public class void main(String args[]){
+    ArrayList<Sting> FoodList=new   ArrayList<Sting>();
+      list.add=("gulabjamun");
+      list.add=("sweet curd");
+      list.add=("jalebi");
+      list.add=("sandwich");
+      list.add=("pasta");
+    //for descending order using collection sort method with reverse order
+    Collections.sort(FoodList,Collections.reverseOrder());
+    //print the sorted array list
+    System.out.print(FoodList);
+    
+  }
+}
